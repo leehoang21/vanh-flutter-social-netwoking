625228854d5fd782368183c449ee2be669a1726f
KienLe <lkien837@Gmail.com>
2023-02-06 13:35:16 +0700

    Update chat_room.dart

--- lib/finplus/screens/chat_room/chat_room.dart
+++ lib/finplus/screens/chat_room/chat_room.dart
@@ -53,107 +53,104 @@ class ChatRoom extends StatelessWidget {
           ],
         ),
         body: Obx(
-          () {
-            if (c.searchRooms.isEmpty)
-              return SizedBox(
-                child: Center(
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      SvgPicture.asset(
-                        SvgIcon.no_data_icon,
-                      ),
-                      Spaces.box10,
-                      const Text(
-                        'Không có nhóm chat nào',
-                      ),
-                    ],
+          () => c.searchRooms.isEmpty
+              ? SizedBox(
+                  child: Center(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        SvgPicture.asset(
+                          SvgIcon.no_data_icon,
+                        ),
+                        Spaces.box10,
+                        const Text(
+                          'Không có nhóm chat nào',
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-              );
-
-            return CustomSmartRefresher(
-              controller: c.refreshController,
-              onRefresh: c.reload,
-              child: ListView.builder(
-                itemCount: c.searchRooms.length,
-                itemBuilder: (_, i) {
-                  final item = c.searchRooms[i];
-                  return InkWell(
-                    onTap: () {},
-                    child: Padding(
-                      padding: Spaces.a10,
-                      child: Row(
-                        children: [
-                          const Avatar(
-                            value: '',
-                            size: 50,
-                          ),
-                          Spaces.boxW10,
-                          Expanded(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
+                )
+              : CustomSmartRefresher(
+                  controller: c.refreshController,
+                  onRefresh: c.reload,
+                  child: ListView.builder(
+                    itemCount: c.searchRooms.length,
+                    itemBuilder: (_, i) {
+                      final item = c.searchRooms[i];
+                      return InkWell(
+                        onTap: () {},
+                        child: Padding(
+                          padding: Spaces.a10,
+                          child: Row(
+                            children: [
+                              const Avatar(
+                                value: '',
+                                size: 50,
+                              ),
+                              Spaces.boxW10,
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
                                   children: [
-                                    Expanded(
-                                      child: Text(
-                                        item.name,
-                                        overflow: TextOverflow.ellipsis,
-                                        style: TextDefine.P1_M,
-                                      ),
-                                    ),
-                                    Spaces.boxW10,
-                                    _buildTimeMessage(
-                                      DateTime.fromMillisecondsSinceEpoch(
-                                        item.lastMsgTime,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                                Spaces.boxH5,
-                                Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    const Expanded(
-                                      child: Text(
-                                        'Tin nhắn cuối cùng',
-                                        overflow: TextOverflow.ellipsis,
-                                      ),
-                                    ),
-                                    if (item.msgCount != 0)
-                                      Container(
-                                        padding: Spaces.a4,
-                                        alignment: Alignment.center,
-                                        decoration: const BoxDecoration(
-                                          color: Colors.redAccent,
-                                          shape: BoxShape.circle,
-                                        ),
-                                        child: FittedBox(
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Expanded(
                                           child: Text(
-                                            '${item.msgCount > 5 ? '5+' : item.msgCount}',
-                                            style: TextDefine.P4_R.copyWith(
-                                              color: theme.background,
-                                            ),
+                                            item.name,
+                                            overflow: TextOverflow.ellipsis,
+                                            style: TextDefine.P1_M,
                                           ),
                                         ),
-                                      ),
+                                        Spaces.boxW10,
+                                        _buildTimeMessage(
+                                          DateTime.fromMillisecondsSinceEpoch(
+                                            item.lastMsgTime,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    Spaces.boxH5,
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        const Expanded(
+                                          child: Text(
+                                            'Tin nhắn cuối cùng',
+                                            overflow: TextOverflow.ellipsis,
+                                          ),
+                                        ),
+                                        if (item.msgCount != 0)
+                                          Container(
+                                            padding: Spaces.a4,
+                                            alignment: Alignment.center,
+                                            decoration: const BoxDecoration(
+                                              color: Colors.redAccent,
+                                              shape: BoxShape.circle,
+                                            ),
+                                            child: FittedBox(
+                                              child: Text(
+                                                '${item.msgCount > 5 ? '5+' : item.msgCount}',
+                                                style: TextDefine.P4_R.copyWith(
+                                                  color: theme.background,
+                                                ),
+                                              ),
+                                            ),
+                                          ),
+                                      ],
+                                    ),
                                   ],
                                 ),
-                              ],
-                            ),
+                              ),
+                            ],
                           ),
-                        ],
-                      ),
-                    ),
-                  );
-                },
-              ),
-            );
-          },
+                        ),
+                      );
+                    },
+                  ),
+                ),
         ),
       ),
     );
